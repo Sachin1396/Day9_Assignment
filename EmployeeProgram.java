7491a983a2afafd80d4b355990c2d9dc7ffc85d4
Sachin1396 <sachhushetty1396@gmail.com>
2022-06-27 14:05:46 +0530

    Added present or absent code

--- EmployeeProgram.java
+++ EmployeeProgram.java
@@ -2,5 +2,8 @@ public class EmployeeProgram {
     public static void main(String[] args)
     {
         System.out.println("Employee wage cometation program");
+        PresentorAbsent presentorAbsent = new PresentorAbsent();
+        presentorAbsent.present();
+
     }
 }
