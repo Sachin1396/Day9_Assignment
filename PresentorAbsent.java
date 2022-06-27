a484a3d5ba755a9169535888a1a1e6d9975371f2
Sachin1396 <sachhushetty1396@gmail.com>
2022-06-27 14:07:32 +0530

    Added daily wage code

--- PresentorAbsent.java
+++ PresentorAbsent.java
@@ -1,20 +1,20 @@
 public class PresentorAbsent {
     void present()
     {
-       // int wage_per_hour = 20;
-        //int full_day_hour = 8;
-        //int daily_wage;
+        int wage_per_hour = 20;
+        int full_day_hour = 8;
+        int daily_wage;
         int random = (int)(Math.random() * 10)%2;
         if(random==1)
         {
             System.out.println("Employee is present");
-           // daily_wage = wage_per_hour *  full_day_hour ;
-            //System.out.println("daily wage :"+daily_wage);
+            daily_wage = wage_per_hour *  full_day_hour ;
+            System.out.println("daily wage :"+daily_wage);
         }
         else {
             System.out.println("employee is absent");
-          //  daily_wage = 0;
-            //System.out.println("Daily wage:"+daily_wage);
+            daily_wage = 0;
+            System.out.println("Daily wage:"+daily_wage);
         }
 
     }
