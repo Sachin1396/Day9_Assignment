aaecac14dfd18afe927de6527794028a500fbf09
Sachin1396 <sachhushetty1396@gmail.com>
2022-06-27 14:12:29 +0530

    Added parttime code

--- PresentorAbsent.java
+++ PresentorAbsent.java
@@ -3,18 +3,24 @@ public class PresentorAbsent {
     {
         int wage_per_hour = 20;
         int full_day_hour = 8;
+        int part_time = 4;
         int daily_wage;
-        int random = (int)(Math.random() * 10)%2;
+        int random = (int)(Math.random() * 10)%3;
         if(random==1)
         {
             System.out.println("Employee is present");
             daily_wage = wage_per_hour *  full_day_hour ;
             System.out.println("daily wage :"+daily_wage);
-        }
-        else {
-            System.out.println("employee is absent");
+        } else if (random==2) {
+        System.out.println("employee is absent");
             daily_wage = 0;
             System.out.println("Daily wage:"+daily_wage);
+        }
+        else {
+            System.out.println("employee is Partime");
+            daily_wage = wage_per_hour * part_time;
+            System.out.println("Daily wage:"+daily_wage);
+
         }
 
     }
